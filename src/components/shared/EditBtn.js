8b8ae7e235e5f852133d3a63c5bfeaad41d68e6b
Shin <scho524@gmail.com>
2021-02-09 12:29:01 -0600

    add EditBtn and ModalForm components

---
+++ src/components/shared/EditBtn.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { faEdit } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { media } from '../commonStyles';
+
+function EditBtn({onClick}) {
+    return (
+        <StyledBtnWrapper 
+            onClick={onClick} 
+            aria-role="button" 
+            aria-label="edit"
+            title="Edit"
+        >
+            <FontAwesomeIcon icon={faEdit} />
+        </StyledBtnWrapper>
+    )
+}
+
+export default EditBtn
+
+const StyledBtnWrapper = styled.span`
+    font-size: .75rem;
+    color: var(--med-lite-grey);
+    background-color: transparent;
+    cursor: pointer;
+    @media(min-width: ${media.medium}) {
+        font-size: 1rem;
+    }
+`
---
+++ src/components/shared/ModalForm.js
@@ -0,0 +1,52 @@
+import { CSSTransition } from 'react-transition-group';
+import styled from 'styled-components'
+
+import useToggle from '../../hooks/toggle';
+import { Form, CancelButton, SubmitButton, PopInStyles } from "../commonStyles";
+import Modal from "./Modal";
+
+function ModalForm(props) {
+    const { open, toggleOpen, onChange, onSubmit } = props;
+    const [isIn, toggleIsIn] = useToggle(true);
+    const delayToggleModal = () => {
+        toggleIsIn();
+        setTimeout(toggleOpen, 200)
+    }
+    return (
+        <Modal modalOpen={open} toggleModal={delayToggleModal}>
+            <CSSTransition 
+                in={isIn}
+                timeout={200}
+                classNames="pop-in"
+                appear={true}
+                unmountOnExit={true}
+            >
+                <StyledForm onChange={onChange} onSubmit={onSubmit}>
+                    {props.children}
+                    <Buttons>
+                        <CancelButton onClick={delayToggleModal} type="button">Cancel</CancelButton>
+                        <SubmitButton type="submit">Save Changes</SubmitButton>
+                    </Buttons>
+                </StyledForm>
+            </CSSTransition>
+        </Modal>
+    )
+}
+
+export default ModalForm;
+
+const StyledForm = styled(Form)`
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    max-width: 400px;
+    ${PopInStyles}
+`
+
+const Buttons = styled.div`
+    display: grid;
+    grid-template-columns: minmax(10px, 1fr) minmax(10px, 1fr);
+    gap: .5rem;
+`
